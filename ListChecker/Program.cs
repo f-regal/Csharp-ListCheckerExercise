a896cba6d1342a0012e0ac33f567290381935878
Faysal Regal <faysalregal@gmail.com>
2022-07-26 20:11:57 +0100

    first ine

---
+++ ListChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListChecker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!1");
+        }
+    }
+}
