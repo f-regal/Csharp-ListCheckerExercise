ab76c9d7f4710972b9ccbe305b26b1dfdd45b370
Faysal Regal <faysalregal@gmail.com>
2022-07-26 20:35:20 +0100

    update list checker

--- ListChecker/Program.cs
+++ ListChecker/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListChecker
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!1");
+            //  Write a program and ask the user to supply a list of comma separated numbers(e.g 5, 1, 9, 2, 10).
+            //  If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
+            //  otherwise, display the 3 smallest numbers in the list.
+
+            Console.WriteLine("Please enter a list of 5 comma seperated numbers");
+
+            //var userInput = Console.ReadLine();
+
+            //List<string> Input = new List<string>();
+
+            //for (var i = 0; i < 5; i++)
+            //{
+            //    Input.Add(userInput[i].ToString());
+            //};
+
+            //Console.WriteLine(Input);
+            
         }
     }
 }
