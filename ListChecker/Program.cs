d03bf998074e9aaee87835ed6a504b1bbd6398c1
Faysal Regal <faysalregal@gmail.com>
2022-08-02 20:39:56 +0100

    Added sorting method

--- ListChecker/Program.cs
+++ ListChecker/Program.cs
@@ -11,25 +11,49 @@ namespace ListChecker
             //  If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
             //  otherwise, display the 3 smallest numbers in the list.
 
-            Console.WriteLine("Please enter a list of 5 comma seperated numbers");
-
-            var userInput = Console.ReadLine();
-
-            string[] inputs = userInput.Split(',');
-
-            if (String.IsNullOrEmpty(userInput) || inputs.Length < 5)
+            string[] inputs;
+            while (true)
             {
-                Console.WriteLine("Invalid, Please Re-try");
-              
-            } else
-            {
+                Console.WriteLine("Please enter a list of 5 comma seperated numbers");
+                var userInput = Console.ReadLine();
+               
+                if (!String.IsNullOrEmpty(userInput))
+                {
+                    inputs = userInput.Split(',');
+                    if (inputs.Length >= 5)
+                    {
+                        break;
+                    }
+                }
 
+                Console.WriteLine("Invalid Please Try Again");
             }
 
+            var numbers = new List<int>();
+            foreach (string input in inputs)
+            {
+                numbers.Add(Convert.ToInt32(input));
+            }
 
+            var smallest = new List<int>();
+            while (smallest.Count < 3)
+            {
+                // Assume the first number is the smallest
+                var min = numbers[0];
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                        min = number;
+                }
+                smallest.Add(min);
 
+                numbers.Remove(min);
+            }
 
-
+            foreach (var num in smallest)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
